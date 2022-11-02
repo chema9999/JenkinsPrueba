869b687d9e0d640e10ab059768666d5cb861f7e7
chema_ <jomajuba@gmail.com>
2022-11-02 15:27:43 +0100

    Adding new files

---
+++ pruebaJ.java
@@ -0,0 +1,10 @@
+package pruebaJenkins;
+
+public class pruebaJ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World benbenuti a la felicita");
+	}
+
+}
